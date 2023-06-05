d75ac2c0d4531baeeca0f0c374b8af1fe88980aa
Dmitriy Kardava <dmitriy.kardava@gmail.com>
2023-06-05 13:53:00 +0300

    decorate process

--- lesson4/img_save_processes.py
+++ lesson4/img_save_processes.py
@@ -1,26 +1,18 @@
 from multiprocessing import Process
-from tools import create_img_dir, get_urls  # , save_url, timeit
-from time import time
-import requests
+from tools import create_img_dir, get_urls, save_url, timeit
 
 
-def save_url(url):
-    _start = time()
-    response = requests.get(url)
-    filename = f'{url.split("/")[-1]}'
-    with open(f'img/{filename}', 'wb') as f:
-        f.write(response.content)
-    print(f'{url=}')
-    print(f'Executed in {(time() - _start):.4f}s')
+def save_process(**kwargs):
+    timeit(save_url(url=kwargs.get('url')))
 
 
-# @timeit
+@timeit
 def url_save_processes():
     create_img_dir()
     processes = []
     urls = get_urls()
     for url in urls:
-        process = Process(target=save_url, kwargs={'url': url})
+        process = Process(target=save_process, kwargs={'url': url})
         processes.append(process)
         process.start()
 
@@ -31,6 +23,4 @@ def url_save_processes():
 
 
 if __name__ == '__main__':
-    start = time()
     url_save_processes()
-    print(f'Executed in {(time() - start):.4f}s')
